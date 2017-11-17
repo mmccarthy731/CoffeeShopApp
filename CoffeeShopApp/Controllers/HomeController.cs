fe3a35c1f3f5efe229e7fb14f355d94f3a361de1
Michael R. McCarthy <mmccarthy731@yahoo.com>
2017-11-17 08:55:19 -0500

    first commit

---
+++ CoffeeShopApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeShopApp.Models;
+
+namespace CoffeeShopApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult UserRegistration()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterNewUser(User NewUser)
+        {
+                ViewBag.FirstName = NewUser.FirstName;
+                return View();
+        }
+    }
+}
---
+++ CoffeeShopApp/Models/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeShopApp.Models
+{
+    public class User
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string phoneNumber;
+        private string password;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public User() : this("", "", "", "", "")
+        {
+
+        }
+
+        public User(string firstName, string lastName, string email, string phoneNumber, string password)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+            this.password = password;
+        }
+    }
+}
