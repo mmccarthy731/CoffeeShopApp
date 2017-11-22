8e8bdbf7c53c40b0742ff3883fe19c7f96c4d6fb
Michael R. McCarthy <mmccarthy731@yahoo.com>
2017-11-22 10:29:39 -0500

    linked with DB

--- CoffeeShopApp/Controllers/HomeController.cs
+++ CoffeeShopApp/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CoffeeShopApp.Models;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CoffeeShopApp.Controllers
 {
@@ -14,20 +16,6 @@ namespace CoffeeShopApp.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult UserRegistration()
         {
             return View();
@@ -35,8 +23,79 @@ namespace CoffeeShopApp.Controllers
 
         public ActionResult RegisterNewUser(User NewUser)
         {
-                ViewBag.FirstName = NewUser.FirstName;
-                return View();
+            if (ModelState.IsValid)
+            {
+                CoffeeShopEntities ORM = new CoffeeShopEntities();
+
+                ORM.Users.Add(NewUser);
+                ORM.SaveChanges();
+
+                return View("Index");
+            }
+            else
+            {
+                return RedirectToAction("UserRegistration");
+            }
+        }
+
+        public ActionResult ListProducts()
+        {
+            CoffeeShopEntities ORM = new CoffeeShopEntities();
+
+            List<Item> productList = ORM.Items.ToList();
+
+            ViewBag.ProductList = productList;
+
+            return View("ListProducts");
+        }
+
+        public ActionResult DeleteProduct(string name)
+        {
+            CoffeeShopEntities ORM = new CoffeeShopEntities();
+
+            Item itemToDelete = ORM.Items.Find(name);
+            
+            if (itemToDelete != null)
+            {
+                ORM.Items.Remove(itemToDelete);
+                ORM.SaveChanges();
+            }
+
+            return RedirectToAction("ListProducts");
+        }
+
+        public ActionResult UpdateProduct(string name)
+        {
+            CoffeeShopEntities ORM = new CoffeeShopEntities();
+
+            Item itemToUpdate = ORM.Items.Find(name);
+
+            if (itemToUpdate != null)
+            {
+                ViewBag.ItemToUpdate = itemToUpdate;
+
+                return View("UpdateItemForm");
+            }
+            else
+            {
+                return RedirectToAction("ListProduts");
+            }
+        }
+
+        public ActionResult SaveUpdatedItem(Item itemToUpdate)
+        {
+            CoffeeShopEntities ORM = new CoffeeShopEntities();
+
+            Item temp = ORM.Items.Find(itemToUpdate.Name);
+
+            temp.Description = itemToUpdate.Description;
+            temp.Quantity = itemToUpdate.Quantity;
+            temp.Price = itemToUpdate.Price;
+
+            ORM.Entry(temp).State = System.Data.Entity.EntityState.Modified;
+            ORM.SaveChanges();
+
+            return RedirectToAction("ListProducts");
         }
     }
 }
